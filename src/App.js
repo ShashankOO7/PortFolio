7a7b24c124e6877df6169b5b7ae915a75745e12a
ShashankOO7 <Shashankchourasia7@gmail.com>
2024-05-19 11:37:58 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
